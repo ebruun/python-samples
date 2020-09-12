746e5090f45659c60f01bf831a0308966d713b21
EdvardBruun <ebruun@princeton.edu>
2020-09-12 16:28:11 -0400

    messing around with capture_from_file

--- source/camera/basic/capture_from_file.py
+++ source/camera/basic/capture_from_file.py
@@ -1,15 +1,17 @@
 """File camera capture sample."""
 import zivid
 
-
 def _main():
+    print("heyyooo")
+    print(zivid.environment.data_path())
     app = zivid.Application()
     camera = app.create_file_camera(
         str(zivid.environment.data_path()) + "/MiscObjects.zdf"
     )
 
+    # Dpes not allow you to save anywhere else...(EB NOTE)
     with camera.capture() as frame:
-        frame.save("Result.zdf")
+        frame.save("results.zdf") 
 
 
 if __name__ == "__main__":
